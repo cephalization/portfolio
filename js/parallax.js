8cc31be14ed3d032d22a7177d2e412e52d59a65b
cephalization <tony2gamer@gmail.com>
2015-08-19 12:46:57 -0400

    Initial styling additions. Copied and modified parallax styling and script.

---
+++ js/parallax.js
@@ -0,0 +1,23 @@
+//Script grabbed from http://untame.net/2013/04/how-to-integrate-simple-parallax-with-twitter-bootstrap/
+//Thanks internet!
+$(document).ready(function(){
+   // cache the window object
+   $window = $(window);
+
+   $('section[data-type="background"]').each(function(){
+     // declare the variable to affect the defined data-type
+     var $scroll = $(this);
+
+      $(window).scroll(function() {
+        // HTML5 proves useful for helping with creating JS functions!
+        // also, negative value because we're scrolling upwards
+        var yPos = -($window.scrollTop() / $scroll.data('speed'));
+
+        // background position
+        var coords = '50% '+ yPos + 'px';
+
+        // move the background
+        $scroll.css({ backgroundPosition: coords });
+      }); // end window scroll
+   });  // end section function
+}); // close out script
